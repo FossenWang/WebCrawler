880cb8ab3c273ce7f1fb8d1fa5ecaabe91d9200c
FossenWang <31503932+FossenWang@users.noreply.github.com>
2017-12-04 20:44:45 +0800

    fix

--- bilibili.py
+++ bilibili.py
@@ -7,7 +7,7 @@ import requests
 from django.utils import timezone
 from PIL import Image
 
-#这里导入的是我另外开发的一个的django网站中的模型，用于保存数据至数据库，有时导入Video类的路径可能会与下面不同
+#这里导入的是我另外开发的一个的基于django开发的网站中的模型，用于保存数据至数据库，有时导入Video类的路径可能会与下面不同
 from DjangoBlog.video.models import Video
 
 SUBMITVIDEOS = 'http://space.bilibili.com/ajax/member/getSubmitVideos'
@@ -39,8 +39,8 @@ def get_submit_videos(mid, pagesize=200, tid=0, page=1, keyword='', order='pubda
             img.save('F:/Project/Python/DjangoBlog/media/video/cover/'+video['pic'].split('/')[-1],'jpeg')
             cover ='video/cover/'+video['pic'].split('/')[-1]
         except OSError as e:
-            cover ='public/images/nonpic.jpg'
-            print('无法获取该视频的图片：aid='+str(video['aid']))
+            cover = 'public/images/nonpic.jpg'
+            print(e, '无法获取该视频的图片：aid='+str(video['aid']))
         video_model = Video(title=title, video=video_url, video_length=length, cover=cover, note=description, pub_date=pub_date)
         video_model.save()
         i+=1
