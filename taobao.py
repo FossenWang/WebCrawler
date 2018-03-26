70bacf283a9a36deeb0c4e6f903d062d3a699408
FossenWang <31503932+FossenWang@users.noreply.github.com>
2018-03-26 16:25:52 +0800

    3

--- taobao.py
+++ taobao.py
@@ -1,27 +1,34 @@
 from time import sleep
 from random import uniform
-import requests, re
+import os, sys, requests, re, traceback
 
-from selenium.webdriver.chrome.webdriver import WebDriver as Chrome
-from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.webdriver import WebDriver as Firefox
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.common.keys import Keys
 
+from django import setup
 from django.utils import timezone
+from django.core.mail import send_mail
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "storage.settings")
+    setup()
+
 from storage.models import TaobaoShop, TaobaoItem, TaobaoItemRecord
 
 # driver.execute_script('window.open()')
 # driver.window_handles
-TIMEOUT = (3, 6)
 
-def rest(rest=TIMEOUT):
+def rest(rest=(3, 7)):
     sleep(uniform(*rest))
+
 
 class TaoBaoVisitor():
     '浏览淘宝网'
     def __init__(self):
-        chrome_options = ChromeOptions()
-        #chrome_options.set_headless()
-        self.driver = Chrome(chrome_options=chrome_options)
+        firefox_options = FirefoxOptions()
+        firefox_options.set_headless()
+        self.driver = Firefox(firefox_options=firefox_options)
         self.driver.implicitly_wait(5)
         self.shops = None
 
@@ -29,12 +36,14 @@ class TaoBaoVisitor():
         self.driver.quit()
 
     def get_shops(self):
+        '获取收藏的店铺'
         if self.shops:
             return self.shops
         else:
             return TaobaoShop.objects.all()
 
     def get_all_items(self):
+        '获取今日所有商品和价格'
         if not self.shops:
             self.shops = self.get_shops()
         for shop in self.shops:
@@ -43,12 +52,15 @@ class TaoBaoVisitor():
                 self.get_taobao_shop_items(shop)
             elif 'tmall' in shop_items_url:
                 self.get_tmall_shop_items(shop)
+            rest([180, 420])
 
     def get_taobao_shop_items(self, shop):
+        '获取淘宝店铺中的商品及其价格'
         driver = self.driver
         driver.get(shop.get_list_url())
         shop_search_result = driver.find_elements_by_id('J_ShopSearchResult')[0]
         if shop.keyword:
+            rest()
             input_keyword = shop_search_result.find_elements_by_name('keyword')[0]
             input_keyword.send_keys(shop.keyword)
             input_keyword.send_keys(Keys.RETURN)
@@ -56,44 +68,23 @@ class TaoBaoVisitor():
         while next_page:
             shop_search_result = driver.find_elements_by_id('J_ShopSearchResult')[0]
             items = shop_search_result.find_elements_by_css_selector('.item')
-            new_items = []
-            new_records = []
-            for item in items:
-                item_id = item.get_attribute('data-id')
-                if not TaobaoItem.objects.filter(id=item_id).exists():
-                    new_items.append(TaobaoItem(
-                        id=item_id,
-                        name=item.find_elements_by_css_selector('.detail>.item-name')[0].text,
-                        shop_id=shop.id
-                    ))
-                today = timezone.now().date()
-                if not TaobaoItemRecord.objects.filter(
-                    item_id=item_id,
-                    date=today,
-                    ).exists():
-                    new_records.append(TaobaoItemRecord(
-                        item_id=item_id,
-                        date=today,
-                        price=float(item.find_elements_by_css_selector('.detail .c-price')[0].text)
-                    ))
-            TaobaoItem.objects.bulk_create(new_items)
-            TaobaoItemRecord.objects.bulk_create(new_records)
-            print(new_items)
-            print(new_records)
+            self.collect_item_datas(items, shop)
             pagination_a = shop_search_result.find_elements_by_css_selector('.pagination-mini>a')
             for a in pagination_a:
                 if a.text=='下一页':
                     if not a.get_attribute('class'):
                         rest()
-                        a.click()
+                        driver.execute_script('arguments[0].click();', a)
                     else:
                         next_page = False
 
     def get_tmall_shop_items(self, shop):
+        '获取淘宝店铺中的商品及其价格'
         driver = self.driver
         driver.get(shop.get_list_url())
         shop_search_result = driver.find_elements_by_id('J_ShopSearchResult')[0]
         if shop.keyword:
+            rest()
             input_keyword = shop_search_result.find_elements_by_name('keyword')[0]
             input_keyword.send_keys(shop.keyword)
             input_keyword.send_keys(Keys.RETURN)
@@ -102,45 +93,47 @@ class TaoBaoVisitor():
             shop_search_result = driver.find_elements_by_id('J_ShopSearchResult')[0]
             j_titems = shop_search_result.find_elements_by_css_selector('.J_TItems>div')
             items = []
-            new_items = []
-            new_records = []
             for div in j_titems:
                 if div.get_attribute('class')=='pagination':
                     break
                 else:
-                    items+=div.find_elements_by_css_selector('.item')
-            for item in items:
-                item_id = item.get_attribute('data-id')
-                if not TaobaoItem.objects.filter(id=item_id).exists():
-                    new_items.append(TaobaoItem(
-                        id=item_id,
-                        name=item.find_elements_by_css_selector('.detail>.item-name')[0].text,
-                        shop_id=shop.id
-                    ))
-                today = timezone.now().date()
-                if not TaobaoItemRecord.objects.filter(
-                    item_id=item_id,
-                    date=today,
-                    ).exists():
-                    new_records.append(TaobaoItemRecord(
-                        item_id=item_id,
-                        date=today,
-                        price=float(item.find_elements_by_css_selector('.detail .c-price')[0].text)
-                    ))
-            TaobaoItem.objects.bulk_create(new_items)
-            TaobaoItemRecord.objects.bulk_create(new_records)
-            print(new_items)
-            print(new_records)
+                    items += div.find_elements_by_css_selector('.item')
+            self.collect_item_datas(items, shop)
             pagination_a = shop_search_result.find_elements_by_css_selector('.ui-page-s>a')
             next_page = False
             for a in pagination_a:
                 if a.text=='>':
                     next_page = True
                     rest()
-                    a.click()
+                    driver.execute_script('arguments[0].click();', a)
+        
+    def collect_item_datas(self, items, shop):
+        '收集商品数据并保存至数据库'
+        new_items = []
+        new_records = []
+        for item in items:
+            item_id = item.get_attribute('data-id')
+            if not TaobaoItem.objects.filter(id=item_id).exists():
+                new_items.append(TaobaoItem(
+                    id=item_id,
+                    name=item.find_elements_by_css_selector('.detail>.item-name')[0].text,
+                    shop_id=shop.id
+                ))
+                print(new_items[-1])
+            today = timezone.now().date()
+            if not TaobaoItemRecord.objects.filter(item_id=item_id, date=today).exists():
+                new_records.append(TaobaoItemRecord(
+                    item_id=item_id,
+                    date=today,
+                    price=float(item.find_elements_by_css_selector('.detail .c-price')[0].text)
+                ))
+                print(new_records[-1])
+        TaobaoItem.objects.bulk_create(new_items)
+        TaobaoItemRecord.objects.bulk_create(new_records)
 
 
-def add_shop(url, keyword):
+def add_shop(url, keyword=None):
+    '添加要追踪的店铺'
     url = url.split('com')[0] + 'com/'
     r = requests.get(url)
     match = re.search(r'shopId=([0-9]*)', r.text)
@@ -154,7 +147,19 @@ def add_shop(url, keyword):
         print('无法找到该店铺')
 
 
+def send_email(title, message):
+    send_mail(
+        title,
+        message,
+        'admin@fossen.cn',
+        ['fossen@fossen.cn']
+    )#在setting.py中定义邮箱与密码
+
+
 if __name__ == "__main__":
-    v = TaoBaoVisitor()
-    v.get_all_items()
+    try:
+        v = TaoBaoVisitor()
+        v.get_all_items()
+    except Exception as e:
+        send_email('淘宝爬虫出错', traceback.format_exc())
     v.quit()
